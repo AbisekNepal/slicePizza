47d814457c5736421bfda541549c98b4a8033116
abisek.dev <92259329+AbisekNepal@users.noreply.github.com>
2024-12-26 19:30:20 -0500

    Create dominosList_model.dart

---
+++ dominosList_model.dart
@@ -0,0 +1,18 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'dominos_list_widget.dart' show DominosListWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class DominosListModel extends FlutterFlowModel<DominosListWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
