0b2148d31fa6629b5f8149fb22e7c66389ff6fd4
abisek.dev <92259329+AbisekNepal@users.noreply.github.com>
2024-12-26 19:33:30 -0500

    Create HomePage1_model.dart

---
+++ HomePage1_model.dart
@@ -0,0 +1,45 @@
+import '/flutter_flow/flutter_flow_button_tabbar.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'home_page1_widget.dart' show HomePage1Widget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class HomePage1Model extends FlutterFlowModel<HomePage1Widget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  // State field(s) for emailAddress_Login widget.
+  FocusNode? emailAddressLoginFocusNode;
+  TextEditingController? emailAddressLoginTextController;
+  String? Function(BuildContext, String?)?
+      emailAddressLoginTextControllerValidator;
+  // State field(s) for password_Login widget.
+  FocusNode? passwordLoginFocusNode;
+  TextEditingController? passwordLoginTextController;
+  late bool passwordLoginVisibility;
+  String? Function(BuildContext, String?)? passwordLoginTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {
+    passwordLoginVisibility = false;
+  }
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+    emailAddressLoginFocusNode?.dispose();
+    emailAddressLoginTextController?.dispose();
+
+    passwordLoginFocusNode?.dispose();
+    passwordLoginTextController?.dispose();
+  }
+}
